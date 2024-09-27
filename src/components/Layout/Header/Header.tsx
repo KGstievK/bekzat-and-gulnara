7f8f2ba29fd8363ac0b7287ca7547d02d540aa4c
KGstievK <kagastiev.kudaibergen.kk@gmail.com>
2024-09-27 19:14:06 +0600

    Добавлен аудиофайл

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,30 +1,29 @@
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useRef } from "react";
 import scss from "./Header.module.scss";
 
 const Header = () => {
-  const [isPlaying, setIsPlaying] = useState(true); // Состояние воспроизведения
-  const audioRef = useRef<HTMLAudioElement>(null); // Ссылка на аудио элемент
+  const audioRef = useRef<HTMLAudioElement>(null);
 
-  // Эффект для автозапуска аудио при загрузке сайта
   useEffect(() => {
-    if (audioRef.current) {
-      audioRef.current.play().catch((error) => {
-        console.log("Автозапуск аудио заблокирован браузером.", error);
-      });
-    }
-  }, []);
-
-  // Функция для переключения воспроизведения
-  const togglePlay = () => {
-    if (audioRef.current) {
-      if (isPlaying) {
-        audioRef.current.pause(); // Остановка
-      } else {
-        audioRef.current.play(); // Воспроизведение
+    const handleUserInteraction = () => {
+      if (audioRef.current) {
+        audioRef.current.volume = 0.2; // Установка громкости
+        audioRef.current.play().catch((error) => {
+          console.log("Не удалось воспроизвести аудио:", error);
+        });
       }
-      setIsPlaying(!isPlaying); // Обновление состояния
-    }
-  };
+      // Удаляем обработчик после первого клика
+      document.removeEventListener("click", handleUserInteraction);
+    };
+
+    // Добавляем обработчик события клика на документ
+    document.addEventListener("click", handleUserInteraction);
+
+    // Убираем обработчик при размонтировании компонента
+    return () => {
+      document.removeEventListener("click", handleUserInteraction);
+    };
+  }, []);
 
   return (
     <header className={scss.Header}>
@@ -32,13 +31,18 @@ const Header = () => {
         <div className={scss.content}>
           <div className={scss.modalOverlay}>
             <div className={scss.modalContent}>
-              <audio ref={audioRef} controls>
+              <audio ref={audioRef}>
                 <source
                   src="/audio/05-EdSheeran-Perfect.mp3"
                   type="audio/mpeg"
                 />
-                Ваш браузер не поддерживает элемент аудио.
+                <source
+                  src="/audio/05-EdSheeran-Perfect.ogg"
+                  type="audio/ogg"
+                />
+                Ваш браузер не поддерживает аудио.
               </audio>
+              <p>Нажмите где угодно, чтобы запустить музыку.</p>
             </div>
           </div>
         </div>
